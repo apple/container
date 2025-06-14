96ca7ce328ea4fd7a6a3a01594e416051c902ca5
ramsyana <47033578+ramsyana@users.noreply.github.com>
2025-06-14 14:01:30 +0800

    chore: fix formatting and license header for TestCLIRmRace.swift (#130)
    Signed-off-by: ramsyana <47033578+ramsyana@users.noreply.github.com>

--- Tests/CLITests/Subcommands/Containers/TestCLIRmRace.swift
+++ Tests/CLITests/Subcommands/Containers/TestCLIRmRace.swift
@@ -59,19 +59,18 @@ class TestCLIRmRaceCondition: CLITest {
             // Call doRemove again with retry logic for cleanup
             var removeAttempts = 0
             let maxRemoveAttempts = 5
-            let removeDelay = 2.0 // seconds
-            
+            let removeDelay = 2.0  // seconds
+
             while removeAttempts < maxRemoveAttempts {
                 do {
                     try doRemove(name: name)
                     break
                 } catch {
-                    if removeAttempts < maxRemoveAttempts - 1 {
-                        try await Task.sleep(for: .seconds(removeDelay * pow(2.0, Double(removeAttempts))))
-                        removeAttempts += 1
-                    } else {
+                    guard removeAttempts < maxRemoveAttempts - 1 else {
                         throw error
                     }
+                    try await Task.sleep(for: .seconds(removeDelay * pow(2.0, Double(removeAttempts))))
+                    removeAttempts += 1
                 }
             }
 
