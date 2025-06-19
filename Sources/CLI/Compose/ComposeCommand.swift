316e136c43000efe027b6e66e8703eac1e9befb4
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-06-19 02:27:23 -0700

    Formatting fixes

--- Sources/CLI/Compose/ComposeCommand.swift
+++ Sources/CLI/Compose/ComposeCommand.swift
@@ -1,3 +1,19 @@
+//===----------------------------------------------------------------------===//
+// Copyright © 2025 Apple Inc. and the container project authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===----------------------------------------------------------------------===//
+
 //
 //  File.swift
 //  Container-Compose
@@ -5,9 +21,9 @@
 //  Created by Morris Richman on 6/18/25.
 //
 
+import ArgumentParser
 import Foundation
 import Yams
-import ArgumentParser
 
 struct ComposeCommand: AsyncParsableCommand {
     static let configuration: CommandConfiguration = .init(
@@ -15,7 +31,7 @@ struct ComposeCommand: AsyncParsableCommand {
         abstract: "A tool to use manage Docker Compose files with Apple Container",
         subcommands: [
             ComposeUp.self,
-            ComposeDown.self
+            ComposeDown.self,
         ])
 }
 
--- Sources/CLI/Compose/Errors.swift
+++ Sources/CLI/Compose/Errors.swift
@@ -1,3 +1,19 @@
+//===----------------------------------------------------------------------===//
+// Copyright © 2025 Apple Inc. and the container project authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===----------------------------------------------------------------------===//
+
 //
 //  Errors.swift
 //  Container-Compose
@@ -9,7 +25,7 @@ import Foundation
 
 enum YamlError: Error, LocalizedError {
     case dockerfileNotFound(String)
-    
+
     var errorDescription: String? {
         switch self {
         case .dockerfileNotFound(let path):
@@ -21,7 +37,7 @@ enum YamlError: Error, LocalizedError {
 enum ComposeError: Error, LocalizedError {
     case imageNotFound(String)
     case invalidProjectName
-    
+
     var errorDescription: String? {
         switch self {
         case .imageNotFound(let name):
@@ -34,9 +50,9 @@ enum ComposeError: Error, LocalizedError {
 
 enum TerminalError: Error, LocalizedError {
     case commandFailed(String)
-    
+
     var errorDescription: String? {
-        return "Command failed: \(self)"
+        "Command failed: \(self)"
     }
 }
 
