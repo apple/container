24f05a2ac5aa0b56f56aeae3dd076b99e6fb2565
Danny Canter <danny_canter@apple.com>
2025-07-07 21:08:16 -0700

    CLI: Add --platform flag
    Fixes #231
    
    --platform is useful as it's what most folks are used to
    from other container tools. We support two separate flags today
    that when combined (--os and --arch) do essentially the same thing,
    but this is a combined single string form.

--- Sources/ContainerClient/Flags.swift
+++ Sources/ContainerClient/Flags.swift
@@ -112,6 +112,9 @@ public struct Flags {
             name: [.customLong("arch"), .customShort("a")], help: "Set arch if image can target multiple architectures")
         public var arch: String = Arch.hostArchitecture().rawValue
 
+        @Option(name: .customLong("platform"), help: "Platform for the image if it's multi-platform")
+        public var platform: String?
+
         @Option(name: [.customLong("volume"), .customShort("v")], help: "Bind mount a volume into the container")
         public var volumes: [String] = []
 
