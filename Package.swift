159ce20034d9b9c5f58be2d2f53192eeb391b584
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-07-08 23:24:54 -0700

    expose cli fixes

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
         .library(name: "ContainerNetworkService", targets: ["ContainerNetworkService"]),
         .library(name: "ContainerImagesService", targets: ["ContainerImagesService", "ContainerImagesServiceClient"]),
         .library(name: "ContainerClient", targets: ["ContainerClient"]),
-//        .library(name: "ContainerCLI", targets: ["ContainerCLI"]),
+        .executable(name: "ContainerCLI", targets: ["container"]),
         .library(name: "ContainerBuild", targets: ["ContainerBuild"]),
         .library(name: "ContainerLog", targets: ["ContainerLog"]),
         .library(name: "ContainerPersistence", targets: ["ContainerPersistence"]),
@@ -83,26 +83,6 @@ let package = Package(
                 "Rainbow",
             ],
             path: "Sources/CLI"
-        ),
-        .target(
-            name: "ContainerCLI",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Logging", package: "swift-log"),
-                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-                .product(name: "Containerization", package: "containerization"),
-                .product(name: "ContainerizationOCI", package: "containerization"),
-                .product(name: "ContainerizationOS", package: "containerization"),
-                "CVersion",
-                "TerminalProgress",
-                "ContainerBuild",
-                "ContainerClient",
-                "ContainerPlugin",
-                "ContainerLog",
-                "Yams",
-                "Rainbow",
-            ],
-            path: "Sources/ContainerCLI"
         ),
         .executableTarget(
             name: "container-apiserver",
