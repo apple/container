8b4b82bbde45d5f3e5263d2a5efacd21d92d84e1
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-09-18 16:53:10 -0700

    chore(fmt): auto-format before push

--- Sources/ContainerCommands/System/Property/PropertyGet.swift
+++ Sources/ContainerCommands/System/Property/PropertyGet.swift
@@ -14,7 +14,6 @@
 // limitations under the License.
 //===----------------------------------------------------------------------===//
 
-
 import ArgumentParser
 import ContainerClient
 import ContainerPersistence
