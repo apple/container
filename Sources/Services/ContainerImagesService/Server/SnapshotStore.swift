a4f9ff638031890403542b662b2f369a1367f42f
Dmitry Kovba <dkovba@apple.com>
2025-12-09 11:27:32 -0800

    Update Sources/Services/ContainerImagesService/Server/SnapshotStore.swift
    Co-authored-by: J Logan <sgtbakerrahulnet@yahoo.com>

--- Sources/Services/ContainerImagesService/Server/SnapshotStore.swift
+++ Sources/Services/ContainerImagesService/Server/SnapshotStore.swift
@@ -84,7 +84,7 @@ public actor SnapshotStore {
                 throw ContainerizationError(.internalError, message: "missing platform for descriptor \(desc.digest)")
             }
             guard let unpacker = try await self.unpackStrategy(image, platform) else {
-                self.log?.warning("skipping unpack for \(image.reference) for platform \(platform.description). No unpacker configured.")
+                self.log?.warning("no unpacker configured, skipping unpack for \(image.reference) for platform \(platform.description)")
                 continue
             }
             let currentSubTask = await taskManager.startTask()
