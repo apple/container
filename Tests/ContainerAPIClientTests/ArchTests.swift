57e3e340167f75918848e1df1e0f4b6cdce79135
Ronit Sabhaya <ronitsabhaya75@gmail.com>
2026-01-11 09:43:47 -0600

    updated the format for tests

--- Tests/ContainerAPIClientTests/ArchTests.swift
+++ Tests/ContainerAPIClientTests/ArchTests.swift
@@ -1,5 +1,5 @@
 //===----------------------------------------------------------------------===//
-// Copyright © 2025-2026 Apple Inc. and the container project authors.
+// Copyright © 2026 Apple Inc. and the container project authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,45 +18,45 @@ import XCTest
 @testable import ContainerAPIClient
 
 final class ArchTests: XCTestCase {
-    
+
     func testAmd64Initialization() throws {
         let arch = Arch(rawValue: "amd64")
         XCTAssertNotNil(arch)
         XCTAssertEqual(arch, .amd64)
     }
-    
+
     func testX86_64Alias() throws {
         let arch = Arch(rawValue: "x86_64")
         XCTAssertNotNil(arch)
         XCTAssertEqual(arch, .amd64)
     }
-    
+
     func testX86_64WithDashAlias() throws {
         let arch = Arch(rawValue: "x86-64")
         XCTAssertNotNil(arch)
         XCTAssertEqual(arch, .amd64)
     }
-    
+
     func testArm64Initialization() throws {
         let arch = Arch(rawValue: "arm64")
         XCTAssertNotNil(arch)
         XCTAssertEqual(arch, .arm64)
     }
-    
+
     func testCaseInsensitive() throws {
         XCTAssertEqual(Arch(rawValue: "AMD64"), .amd64)
         XCTAssertEqual(Arch(rawValue: "X86_64"), .amd64)
         XCTAssertEqual(Arch(rawValue: "ARM64"), .arm64)
         XCTAssertEqual(Arch(rawValue: "Amd64"), .amd64)
     }
-    
+
     func testInvalidArchitecture() throws {
         XCTAssertNil(Arch(rawValue: "invalid"))
         XCTAssertNil(Arch(rawValue: "i386"))
         XCTAssertNil(Arch(rawValue: "powerpc"))
         XCTAssertNil(Arch(rawValue: ""))
     }
-    
+
     func testRawValueRoundTrip() throws {
         XCTAssertEqual(Arch.amd64.rawValue, "amd64")
         XCTAssertEqual(Arch.arm64.rawValue, "arm64")
