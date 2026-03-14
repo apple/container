f1ecf14cdddc604e97fbecc22a36f1160fb58fd7
Ryan Goulden <percontation@gmail.com>
2026-03-13 17:00:40 -0700

    move --secret parsing into validate()

--- Sources/ContainerCommands/BuildCommand.swift
+++ Sources/ContainerCommands/BuildCommand.swift
@@ -46,6 +46,11 @@ extension Application {
             case tty
         }
 
+        enum SecretType: Decodable {
+            case data(Data)
+            case file(String)
+        }
+
         @Option(
             name: .shortAndLong,
             help: ArgumentHelp("Add the architecture type to the build", valueName: "value"),
@@ -117,6 +122,8 @@ extension Application {
 
         @Option(name: .long, help: ArgumentHelp("Set build-time secrets (format: id=<key>[,env=<ENV_VAR>|,src=<local/path>])", valueName: "id=key,..."))
         var secret: [String] = []
+
+        var secrets: [String: SecretType] = [:]
 
         @Option(name: [.short, .customLong("tag")], help: ArgumentHelp("Name for the built image", valueName: "name"))
         var targetImageNames: [String] = {
@@ -261,30 +268,13 @@ extension Application {
                     }
                 }
 
-                var secretsData: [String: Data] = [:]
-                for secret in self.secret {
-                    let parts = secret.split(separator: ",", maxSplits: 1, omittingEmptySubsequences: false)
-                    guard parts[0].hasPrefix("id=") else {
-                        throw ContainerizationError(.invalidArgument, message: "secret must start with id=<key> \(secret)")
+                let secretsData: [String: Data] = try self.secrets.mapValues { secret in
+                    switch secret {
+                    case .data(let data):
+                        return data
+                    case .file(let path):
+                        return try Data(contentsOf: URL(fileURLWithPath: path))
                     }
-                    let key = String(parts[0].dropFirst(3))
-                    guard !key.contains("=") else {
-                        throw ContainerizationError(.invalidArgument, message: "secret id cannot contain '=' \(key)")
-                    }
-                    let data: Data
-                    if parts.count == 1 || parts[1].hasPrefix("env=") {
-                        let env = parts.count == 1 ? key : String(parts[1].dropFirst(4))
-                        guard let ptr = getenv(env) else {
-                            throw ContainerizationError(.invalidArgument, message: "secret env var doesn't exist \(env)")
-                        }
-                        data = Data(bytes: ptr, count: strlen(ptr))
-                    } else if parts[1].hasPrefix("src=") {
-                        let path = String(parts[1].dropFirst(4))
-                        data = try Data(contentsOf: URL(fileURLWithPath: path))
-                    } else {
-                        throw ContainerizationError(.invalidArgument, message: "secret bad value \(parts[1])")
-                    }
-                    secretsData[key] = data
                 }
 
                 let systemHealth = try await ClientHealthCheck.ping(timeout: .seconds(10))
@@ -492,6 +482,30 @@ extension Application {
 
                 dockerfile = defaultDockerfile
                 break
+            }
+
+            // Parse --secret args
+            for secret in self.secret {
+                let parts = secret.split(separator: ",", maxSplits: 1, omittingEmptySubsequences: false)
+                guard parts[0].hasPrefix("id=") else {
+                    throw ValidationError("secret must start with id=<key> \(secret)")
+                }
+                let key = String(parts[0].dropFirst(3))
+                guard !key.contains("=") else {
+                    throw ValidationError("secret id cannot contain '=' \(key)")
+                }
+                if parts.count == 1 || parts[1].hasPrefix("env=") {
+                    let env = parts.count == 1 ? key : String(parts[1].dropFirst(4))
+                    guard let ptr = getenv(env) else {
+                        throw ValidationError("secret env var doesn't exist \(env)")
+                    }
+                    self.secrets[key] = .data(Data(bytes: ptr, count: strlen(ptr)))
+                } else if parts[1].hasPrefix("src=") {
+                    let path = String(parts[1].dropFirst(4))
+                    self.secrets[key] = .file(path)
+                } else {
+                    throw ValidationError("secret bad value \(parts[1])")
+                }
             }
         }
     }
