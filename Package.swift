09116ccaa5da2c8a867495c48a5fcfd2623aadca
Simone <simone.panico@icloud.com>
2026-05-12 20:57:28 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
         .library(name: "TerminalProgress", targets: ["TerminalProgress"]),
     ],
     dependencies: [
-        .package(path: "~/projects/container/apple-container/containerization"),
+        .package(url: "https://github.com/apple/containerization.git", exact: Version(stringLiteral: scVersion)),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
