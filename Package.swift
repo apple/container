91993c301bdb8d49ece04860d69ffb54e3bb809c
Hamza <12420351+0xMH@users.noreply.github.com>
2026-06-08 23:26:08 +0200

    Add vmnet VPN interface selection tests

--- Package.swift
+++ Package.swift
@@ -348,6 +348,12 @@ let package = Package(
             ],
             path: "Sources/Services/NetworkVmnet/Server"
         ),
+        .testTarget(
+            name: "ContainerNetworkVmnetServerTests",
+            dependencies: [
+                "ContainerNetworkVmnetServer"
+            ]
+        ),
         .target(
             name: "ContainerRuntimeLinuxClient",
             dependencies: [],
