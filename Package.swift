5797ed3651cf0ae4ac5417e957d14a3f4a61919b
somiljain2006 <somil16022006@gmail.com>
2026-06-17 02:40:32 +0530

    Added comma

--- Package.swift
+++ Package.swift
@@ -214,7 +214,7 @@ let package = Package(
                 "ContainerPlugin",
                 "ContainerPersistence",
                 "DNSServer",
-                .product(name: "Logging", package: "swift-log")
+                .product(name: "Logging", package: "swift-log"),
             ]
         ),
         .target(
